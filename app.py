e1ac57fbb070589de165225867001f7f605955fc
Parker Lawrence <parkerlawrence@pal-nat186-46-180.itap.purdue.edu>
2019-09-10 21:58:38 -0400

    second commit

--- app.py
+++ app.py
@@ -14,21 +14,30 @@ def nice():
 	return "nice"
 
 
+elements = []
+
+
+
 @app.route('/elements',methods=["GET","POST"])
 def elements():
 	if request.method == "GET":
-
+		return elements
 	elif request.method == "POST":
+		elements.append(request.json)
+		return elements
+
 
 
 @app.route('/elements/<int:id>',methods=["GET","PUT","DELETE"])
 def element(id):
 	if request.method == "GET":
-
+		return elements[id]
 	elif request.method == "PUT":
-
+		elements[id] = request.json[id]
+		return elements[id]
 	elif request.method == "DELETE":
-		
+		elements.remove(id)
+		return "OK"
 
 
 
